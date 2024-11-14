a2259a52f0ff0eccd6a04c60c7eb9fe5a5a346dd
Andy <1013651@student.sae.edu.au>
2024-11-14 18:09:16 +1000

    New script

---
+++ Assets/Scripts/Door/Combination_Door.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combination_Door : Doors
+{
+    [SerializeField] private List<Interactables> comboCriteria = new List<Interactables>();
+
+    [SerializeField] private int activeCriteria;
+    
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        
+    }
+}
